12b2500a7760a166b791fc595b9cdce46ed14b78
Matthew Klein <mklein994@gmail.com>
2018-05-05 20:53:44 -0500

    Add an authorization controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly IAuthRepository _repo;
+        protected AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string username, string password)
+        {
+            // validate request
+
+            username = username.ToLower();
+
+            if (await _repo.UserExists(username))
+                return BadRequest("Username is already taken");
+
+            var userToCreate = new User
+            {
+                Username = username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, password);
+
+            return StatusCode(201);
+        }
+    }
+}
