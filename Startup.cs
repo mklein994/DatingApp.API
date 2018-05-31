67460f0e96f952baf4f4d1f778d6265ef0ba7d04
Matthew Klein <mklein994@gmail.com>
2018-05-30 21:22:13 -0500

    Disable the seed method
    This can be uncommented to seed the database.

--- Startup.cs
+++ Startup.cs
@@ -82,7 +82,7 @@ namespace DatingApp.API
                     }));
             }
 
-            seeder.SeedUsers();
+            // seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
             app.UseAuthentication();
             app.UseMvc();
